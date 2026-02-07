12d309bbe97f9112deb8719196c55f824df3f67e
Ubuntu <ubuntu@ip-172-31-2-91.ap-south-1.compute.internal>
2026-02-07 06:51:00 +0000

    first app

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "testdb"
+});
+
+db.connect(err => {
+  if(err) throw err;
+  console.log("DB Connected");
+});
+
+app.get('/', (req,res)=>{
+  db.query("SELECT * FROM users", (err,result)=>{
+    if(err) throw err;
+    res.send(result);
+  });
+});
+
+app.listen(3000, ()=>{
+  console.log("Server running on port 3000");
+});
 
